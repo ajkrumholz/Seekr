82d4893472ff033fce6efcce70ff89a2aa86d38e
A.J. Krumholz <krumholz.alan@gmail.com>
2022-12-17 16:34:08 -0500

    add companies query and test

--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -7,7 +7,13 @@ describe 'companies query' do
         { 
           companies 
             {
-
+              companyName
+              description
+              hiringLink
+              rolesHiringFor
+              locationsHiringIn
+              oneNiceThing
+              comments
             }
         }
       GQL
@@ -15,6 +21,28 @@ describe 'companies query' do
 
     companies = create_list(:company, 10)
     post '/graphql', params: { query: fetch_companies }
-    require 'pry'; binding.pry
+
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(result).to have_key(:data)
+    expect(result[:data]).to have_key(:companies)
+    
+    companies = result[:data][:companies]
+    expect(companies).to be_an Array
+    expect(companies.size).to eq 10
+
+    company = companies.first
+    expect(company).to have_key(:companyName)
+    expect(company[:companyName]).to be_a String
+    expect(company).to have_key(:description)
+    expect(company[:description]).to be_a String
+    expect(company).to have_key(:hiringLink)
+    expect(company[:hiringLink]).to be_a String
+    expect(company).to have_key(:rolesHiringFor)
+    expect(company[:rolesHiringFor]).to be_a String
+    expect(company).to have_key(:oneNiceThing)
+    expect(company[:oneNiceThing]).to be_a String
+    expect(company).to have_key(:comments)
+    expect(company[:comments]).to be_a String
   end
 end
