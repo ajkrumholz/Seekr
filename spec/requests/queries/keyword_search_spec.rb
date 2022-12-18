c8df3e1b002f48dc791394c0446930f5598ca25f
A.J. Krumholz <krumholz.alan@gmail.com>
2022-12-18 14:27:51 -0500

    test sad paths for keywordSearch

--- spec/requests/queries/keyword_search_spec.rb
+++ spec/requests/queries/keyword_search_spec.rb
@@ -25,4 +25,50 @@ RSpec.describe 'keyword search query' do
       expect(result[:data][:keywordSearch].size).to eq 3
     end
   end
+
+  describe 'sad path' do
+    describe 'when nothing matches the keyword' do
+      it 'returns an empty array' do
+        def query(keyword)
+          <<~GQL
+            {
+              keywordSearch(keyword: "#{keyword}") {
+                companyName
+                description
+              }
+            }
+          GQL
+        end
+
+        create_list(:company, 10)
+
+        post "/graphql", params: { query: query("$(@FJ#!") }
+        result = JSON.parse(response.body, symbolize_names: true)
+
+        expect(result[:data][:keywordSearch].size).to eq 0
+      end
+    end
+
+    describe 'when keyword is missing' do
+      it 'returns an error' do
+        def query
+          <<~GQL
+            {
+              keywordSearch {
+                companyName
+                description
+              }
+            }
+          GQL
+        end
+
+        create_list(:company, 10)
+
+        post "/graphql", params: { query: query }
+        result = JSON.parse(response.body, symbolize_names: true)
+
+        expect(result[:errors].first[:message]).to eq "Field 'keywordSearch' is missing required arguments: keyword"
+      end
+    end
+  end
 end
