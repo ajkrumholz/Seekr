35f5a4212298c4039c17691226c128863a763fea
A.J. Krumholz <krumholz.alan@gmail.com>
2022-12-18 15:23:53 -0500

    add sad paths for search query

--- spec/requests/queries/search_spec.rb
+++ spec/requests/queries/search_spec.rb
@@ -26,6 +26,28 @@ RSpec.describe 'search query' do
   end
 
   describe 'sad paths' do
+    describe 'when no companies match' do
+      it 'returns an empty array' do
+        create_list(:company, 5, company_name: "travel", roles_hiring_for: "backend")
 
+        def query
+          <<~GQL
+          {
+            search(companyName: "entertainment", rolesHiringFor: "frontend") {
+              companyName
+              description
+            }
+          }
+          GQL
+        end
+
+        post "/graphql", params: { query: query }
+        result = JSON.parse(response.body, symbolize_names: true)
+
+        expect(Company.count).to eq 5
+
+        expect(result[:data][:search]).to be_empty
+      end
+    end
   end
 end
