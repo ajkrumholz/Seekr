91f372d0506770a48b00519e0d844021a7d56795
A.J. Krumholz <krumholz.alan@gmail.com>
2022-12-18 13:35:04 -0500

    test sad and happy paths for company query

--- app/graphql/queries/company.rb
+++ app/graphql/queries/company.rb
@@ -5,5 +5,10 @@ class Queries::Company < Queries::BaseQuery
 
   def resolve(id:)
     Company.find(id)
+    rescue ActiveRecord::RecordNotFound => _e
+      GraphQL::ExecutionError.new("Company doesn't exist.")
+    rescue ActiveRecord::RecordInvalid => e
+      GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
+        " #{e.record.errors.full_messages.join(', ')}")
   end
 end
--- spec/requests/queries/company_spec.rb
+++ spec/requests/queries/company_spec.rb
@@ -2,33 +2,86 @@ require 'rails_helper'
 
 RSpec.describe 'company query' do
   describe 'happy path' do
-    it 'returns information about company with id X' do
-      def query(company_id)
-        <<~GQL
-          {
-            company(id: #{company_id}) {
-              companyName
-              description
-              rolesHiringFor
+    describe 'when an id is provided' do
+      it 'returns information about company with id X' do
+        def query(company_id)
+          <<~GQL
+            {
+              company(id: #{company_id}) {
+                companyName
+                description
+                rolesHiringFor
+              }
             }
-          }
-        GQL
+          GQL
+        end
+
+        company = create :company
+
+        post "/graphql", params: { query: query(company.id) }
+        result = JSON.parse(response.body, symbolize_names: true)
+        
+        expect(result).to have_key(:data)
+
+        company = result[:data][:company]
+        expect(company).to have_key(:companyName)
+        expect(company[:companyName]).to be_a String
+        expect(company).to have_key(:description)
+        expect(company[:description]).to be_a String
+        expect(company).to have_key(:rolesHiringFor)
+        expect(company[:rolesHiringFor]).to be_a String
+      end
+    end
+
+    describe 'sad path' do
+      describe 'when a bad id is provided' do
+        it 'returns an error' do
+          def query(company_id)
+            <<~GQL
+              {
+                company(id: #{company_id}) {
+                  companyName
+                  description
+                  rolesHiringFor
+                }
+              }
+            GQL
+          end
+
+          company = create :company
+
+          post "/graphql", params: { query: query(99999) }
+          result = JSON.parse(response.body, symbolize_names: true)
+
+          expect(result[:data][:company]).to be_nil
+          expect(result[:errors].first[:message]).to eq("Company doesn't exist.")
+        end
       end
 
-      company = create :company
+      describe 'when a bad attribute is requested' do
+        it 'returns an error' do
+          def query(company_id)
+            <<~GQL
+              {
+                company(id: #{company_id}) {
+                  companyName
+                  description
+                  rolesHiringFor
+                  location
+                }
+              }
+            GQL
+          end
 
-      post "/graphql", params: { query: query(company.id) }
-      result = JSON.parse(response.body, symbolize_names: true)
-      
-      expect(result).to have_key(:data)
+          company = create :company
 
-      company = result[:data][:company]
-      expect(company).to have_key(:companyName)
-      expect(company[:companyName]).to be_a String
-      expect(company).to have_key(:description)
-      expect(company[:description]).to be_a String
-      expect(company).to have_key(:rolesHiringFor)
-      expect(company[:rolesHiringFor]).to be_a String
+          post "/graphql", params: { query: query(company.id) }
+          result = JSON.parse(response.body, symbolize_names: true)
+
+          expect(result).not_to have_key(:data)
+          expect(result[:errors].first[:message]).to eq("Field 'location' doesn't exist on type 'Company'")
+        end
+      end
     end
   end
 end
