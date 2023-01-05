95f270d927809098778483ff6d4d442caa186a18
A.J. Krumholz <krumholz.alan@gmail.com>
2023-01-05 18:18:04 -0500

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'https://seekr-mk2.fly.dev/', 'https://seekr-wine.vercel.app/'
+    origins 'localhost:3000', 'https://seekr-mk2.fly.dev/', 'https://seekr-wine.vercel.app'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
